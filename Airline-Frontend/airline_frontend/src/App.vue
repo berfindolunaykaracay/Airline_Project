5a87a46b6f48f953ebdb6387b145f7f56e1c23a3
Mustafa Mert SÜERKAN <msuerkan@partner.innova.com.tr>
2022-07-04 09:48:27 +0300

    element ui eklendi.

--- Airline-Frontend/airline_frontend/src/App.vue
+++ Airline-Frontend/airline_frontend/src/App.vue
@@ -4,7 +4,24 @@
     <router-link to="/about">About</router-link>
   </nav>
   <router-view/>
+  <HelloWorld msg="Welcome to Your Vue.js App"/>
 </template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      value2: ''
+    };
+  }
+}
+</script>
 
 <style>
 #app {
@@ -13,18 +30,6 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  margin-top: 60px;
 }
 </style>
