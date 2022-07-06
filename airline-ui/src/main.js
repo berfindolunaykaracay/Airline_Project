5f0bdb161a5b6ba05a88d4a8c0e4ca9e68176c65
Mustafa Mert SÜERKAN <msuerkan@partner.innova.com.tr>
2022-07-06 10:05:17 +0300

    vuetify eklendi

--- airline-ui/src/main.js
+++ airline-ui/src/main.js
@@ -1,15 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-
-Vue.use(ElementUI);
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
---
+++ airline-ui/src/views/Home.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <v-app id="inspire">
+      <v-timeline>
+        <v-timeline-item>timeline item</v-timeline-item>
+        <v-timeline-item class="text-right">
+          timeline item
+        </v-timeline-item>
+        <v-timeline-item>timeline item</v-timeline-item>
+      </v-timeline>
+    </v-app>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'Home',
+  components: {
+
+  }
+}
+</script>
